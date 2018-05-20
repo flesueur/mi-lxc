e44301d495395f386063b60c1502c20bc2a9b270
flesueur <francois.lesueur@insa-lyon.fr>
2018-05-20 14:46:24 +0200

    infra se lance

---
+++ infra/files/master/pre-provision.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+while [ ! `ip addr show dev eth0 | grep "inet "` ]
+do
+echo Waiting for IP
+sleep 1
+done
+
+ssh-keygen -f /root/.ssh/id_rsa -N ""
+cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install -y openssh-server
+#ssh -o "NoHostAuthenticationForLocalhost True" localhost /tmp/lxc/provision.sh
+
