863a9f564e1b9c08410a6a0a34240935c3f65336
flesueur <francois.lesueur@insa-lyon.fr>
2018-06-08 14:57:07 +0200

    intégration appli php du filer

--- infra/files/filer/provision.sh
+++ infra/files/filer/provision.sh
@@ -3,10 +3,11 @@
 
 if [ -z `hostname | grep lxc-infra` ] ; then exit 1; fi
 
-# DEBIAN_FRONTEND=noninteractive apt-get install -y thunderbird
+DEBIAN_FRONTEND=noninteractive apt-get install -y libapache2-mod-php php-sqlite3 sqlite3
 
-cp /mnt/lxc/filer/index.php /var/www/html/
+cp /mnt/lxc/filer/index.php.sqlite /var/www/html/index.php
 mv /var/www/html/index.html /var/www/html/index.old.html
+sqlite3 /var/www/html/appli.db < /mnt/lxc/filer/db.sqlite
 
 # Disable DHCP and do DNS config
 sed -i "s/.*dhcp.*//" /etc/network/interfaces
