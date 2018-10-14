404f2a91e45a729952fbb69a5136d4e5e78fe96e
flesueur <francois.lesueur@insa-lyon.fr>
2018-10-14 16:40:53 +0200

    added a vagrantfile

---
+++ vagrant/files/provision.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Copie de qques fichiers
+cp /vagrant/files/sources.list /etc/apt/sources.list
+cp /vagrant/files/keyboard /etc/default/keyboard
+cp /vagrant/files/lxc-net /etc/default/lxc-net
+
+
+# MAJ et install
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get -y upgrade
+DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
+DEBIAN_FRONTEND=noninteractive apt-get install -y curl git lxc python3-lxc apache2 vim xfce4 lightdm firefox-esr gnome-terminal firmware-atheros firmware-misc-nonfree tcpdump dsniff whois postgresql wireshark dkms net-tools zerofree # keyboard-configuration  wireshark
+apt-get clean
+
+
+# Localisation fr
+echo "Europe/Paris" > /etc/timezone
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+dpkg-reconfigure -f noninteractive tzdata
+sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i -e 's/# fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/' /etc/locale.gen
+echo 'LANG="fr_FR.UTF-8"'>/etc/default/locale
+dpkg-reconfigure --frontend=noninteractive locales
+update-locale LANG=fr_FR.UTF-8
+
+
+# Creation des utilisateurs
+usermod -p `mkpasswd --method=sha-512 root` root
+useradd -m -s "/bin/bash" -p `mkpasswd --method=sha-512 debian` debian || true
+
+# augmentation de la taille de /run si lowmem
+#echo "tmpfs /run tmpfs nosuid,noexec,size=26M 0  0" >> /etc/fstab
+#mount -o remount /run
+
+#login ssh avec mot de passe
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
+sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/" /etc/ssh/sshd_config
+
+# guest utils et reboot
+VERSION=`curl https://download.virtualbox.org/virtualbox/LATEST-STABLE.TXT`
+curl https://download.virtualbox.org/virtualbox/$VERSION/VBoxGuestAdditions_$VERSION.iso -o /tmp/vbox.iso
+mount -o loop /tmp/vbox.iso /mnt
+/mnt/VBoxLinuxAdditions.run
+
+#/vagrant/files/VBoxLinuxAdditions.run
+
+service lxc restart
+cd /root
+git clone https://github.com/flesueur/mi-lxc
+cd mi-lxc
+./mi-lxc.py create
+
+reboot
