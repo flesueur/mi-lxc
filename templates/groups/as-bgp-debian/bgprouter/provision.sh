0292afc5b2cff3682d75148ff258173276499ab5
Francois Lesueur <francois.lesueur@insa-lyon.fr>
2019-08-10 10:30:07 +0200

    quickfix for target-router bug during create (created a specific as-bgp-debian template)

---
+++ templates/groups/as-bgp-debian/bgprouter/provision.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# BGP router template
+set -e
+if [ -z $MILXCGUARD ] ; then exit 1; fi
+DIR=`dirname $0`
+cd `dirname $0`
+
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install -y bird traceroute
+
+echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+
+
+# get iface name from internal ip
+#iface=`ip route get $asip | awk '{ print $3; exit }'`
+#echo $asip
+#iface=`ifconfig | grep -B1 $asip | grep -o "^\w*"`
+
+# customize bird config
+sed -i "s/router id/router id $asn; #/" /etc/bird/bird.conf
+sed -i "s/\#.*export all/\texport all/" /etc/bird/bird.conf
+if [ ! -z $asdev ]
+ then
+ IFS=';'
+ for i in $asdev; do
+   echo "protocol direct {   interface \"$i\"; } " >> /etc/bird/bird.conf
+ done
+fi
+IFS=';'
+for i in $neighbors; do
+  echo -e "
+protocol bgp {
+  local as $asn;
+  neighbor $i;
+  import all;
+  export all;
+}" >>  /etc/bird/bird.conf
+done
+
+service bird restart
+
+# iptables -t nat -A POSTROUTING -o eth2 ! -d 10.0.0.0/16 -j SNAT --to 192.168.10.1
+# birdc show route all
+# birdc show protocols
