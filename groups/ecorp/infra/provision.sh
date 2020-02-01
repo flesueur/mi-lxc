622336fac369a5bb22794c319593fc335c9ec8ff
Francois Lesueur <francois.lesueur@insa-lyon.fr>
2020-02-01 12:08:49 +0100

    added the ecorp evil AS

---
+++ groups/ecorp/infra/provision.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# ISP-A
+set -e
+if [ -z $MILXCGUARD ] ; then exit 1; fi
+DIR=`dirname $0`
+cd `dirname $0`
+
+# Hacker's mail account hacker@isp-a.milxc
+useradd -m -s "/bin/bash" -p `mkpasswd --method=sha-512 admin` admin || true
+addgroup admin mail
+#mkdir /home/hacker/mail
+#touch /home/hacker/mail/Drafts /home/hacker/mail/Queue /home/hacker/mail/Sent /home/hacker/mail/Trash
+
+# disable systemd-resolved which conflicts with nsd
+echo "DNSStubListener=no" >> /etc/systemd/resolved.conf
+systemctl stop systemd-resolved
+
+# manage isp-a.milxc zone
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install -y unbound
+cp dns.conf /etc/unbound/unbound.conf.d/
--- groups/milxc/ns/provision.sh
+++ groups/milxc/ns/provision.sh
@@ -35,4 +35,6 @@ isp-a.milxc.	IN	NS	ns.isp-a.milxc.
 ns.isp-a.milxc.	IN	A 10.150.1.2
 mica.milxc.	IN	NS	ns.mica.milxc.
 ns.mica.milxc.	IN	A 10.102.0.2
+ecorp.milxc.	IN	NS	ns.ecorp.milxc.
+ns.ecorp.milxc.	IN	A 10.101.0.2
 " >> /etc/nsd/milxc.zone
