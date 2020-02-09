e1e474190d382624540615a3098361be803ef5c0
Francois Lesueur <francois.lesueur@insa-lyon.fr>
2020-02-09 17:06:56 +0100

    added first milxc-autocomplete.bash to autocomplete commandline

---
+++ milxc-completion.bash
@@ -0,0 +1,18 @@
+#/usr/bin/env bash
+
+_milxc_completions()
+{
+  if [ "${#COMP_WORDS[@]}" == "2" ]; then
+      COMPREPLY=($(compgen -W "$(./mi-lxc.py | grep usage | cut -d' ' -f 6- | sed 's/[,.]/ /g' | sed 's/\[[a-z@]*\]//g' | sed 's/<[a-z]*>//g')" "${COMP_WORDS[1]}"))
+      # COMPREPLY=($(compgen -W "create renet destroy destroymaster updatemaster start stop attach display print" "${COMP_WORDS[1]}"))
+      # ./mi-lxc.py | grep usage | cut -d' ' -f 6- | sed 's/, / /g' | sed 's/\[[a-z@]*\]//g' | sed 's/<[a-z]*>//g'
+  fi
+
+  if [ "${#COMP_WORDS[@]}" == "3" ]; then
+      COMPREPLY=($(compgen -W "$(./mi-lxc.py | grep Names | cut -d' ' -f 3- | sed 's/, / /g')" "${COMP_WORDS[2]}"))
+  fi
+
+
+}
+
+complete -F _milxc_completions mi-lxc.py
