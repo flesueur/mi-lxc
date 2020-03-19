b9e24a513989e878a2f35e8ec3db62d6d1c59465
Francois Lesueur <francois.lesueur@insa-lyon.fr>
2020-03-19 18:19:26 +0100

    updated ecorp-infra with certbot and updatecaroots

--- groups/ecorp/infra/provision.sh
+++ groups/ecorp/infra/provision.sh
@@ -5,6 +5,8 @@ if [ -z $MILXCGUARD ] ; then exit 1; fi
 DIR=`dirname $0`
 cd `dirname $0`
 
+DEBIAN_FRONTEND=noninteractive apt-get install -y certbot
+
 # Hacker's mail account hacker@isp-a.milxc
 useradd -m -s "/bin/bash" -p `mkpasswd --method=sha-512 admin` admin || true
 addgroup admin mail
