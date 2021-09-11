96e867d202e100ca9b42c2ed839b90be39ed4454
MorpheusH3x <morpheush3x@protonmail.com>
2021-09-11 23:45:56 +0200

    PEP8 - initial compliance
    Running 'pycodestyle' in order to get all the errors and warning
    regarding to PEP8 Style Guide.
    There is some remaining E & W as:
    - E722: do not use bare 'except'.
    - W503: line break before binary operator.

--- backends/HostBackend.py
+++ backends/HostBackend.py
@@ -2,16 +2,20 @@
 Containerization/Virtualization backend interface
 """
 
+
 class Master:
     """
     This class defines an interface to create masters
     """
     def create(self):
         pass
+
     def update(self):
         pass
+
     def destroy(self):
         pass
+
 
 class Host:
     """
@@ -19,21 +23,30 @@ class Host:
     """
     def create(self):
         pass
+
     def destroy(self):
         pass
+
     def renet(self):
         pass
+
     def exists(self):
         pass
+
     def isRunning(self):
         pass
+
     def start(self):
         pass
+
     def stop(self):
         pass
+
     def display(self, user):
         pass
+
     def attach(self, user, run_command):
         pass
+
     def isReady(self):
         pass
