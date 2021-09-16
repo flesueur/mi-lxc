1f0028e2260e6fcf51fcb40fbecdf9f97e6fcbef
Francois Lesueur <francois.lesueur@insa-lyon.fr>
2021-09-16 15:18:08 +0200

    added createall and destroyall CLI commands, updates usage() and autocompletion accordingly

--- milxc-completion.bash
+++ milxc-completion.bash
@@ -3,13 +3,13 @@
 _milxc_completions()
 {
   if [ "${#COMP_WORDS[@]}" == "2" ]; then
-      COMPREPLY=($(compgen -W "$(./mi-lxc.py | grep usage | cut -d':' -f 2 | sed 's/[,.]/ /g' | sed 's/\[[a-z@]*\]//g' | sed 's/<[a-z]*>//g')" "${COMP_WORDS[1]}"))
-      # COMPREPLY=($(compgen -W "create renet destroy destroymaster updatemaster start stop attach display print" "${COMP_WORDS[1]}"))
+      #COMPREPLY=($(compgen -W "$(./mi-lxc.py | grep usage | cut -d':' -f 2 | sed 's/[,.]/ /g' | sed 's/\[[a-z@]*\]//g' | sed 's/<[a-z]*>//g')" "${COMP_WORDS[1]}"))
+      COMPREPLY=($(compgen -W "create createall renet destroy destroyall destroymaster updatemaster start stop attach display print" "${COMP_WORDS[1]}"))
       # ./mi-lxc.py | grep usage | cut -d' ' -f 6- | sed 's/, / /g' | sed 's/\[[a-z@]*\]//g' | sed 's/<[a-z]*>//g'
   fi
 
   if [ "${#COMP_WORDS[@]}" == "3" ]; then
-      COMPREPLY=($(compgen -W "$(./mi-lxc.py | grep Names | cut -d':' -f 2 | sed 's/, / /g')" "${COMP_WORDS[2]}"))
+      COMPREPLY=($(compgen -W "$(./mi-lxc.py | grep names | cut -d':' -f 2 | sed '{s/, / /g;s/\.$//g}')" "${COMP_WORDS[2]}"))
   fi
 
 
