66fcee5d135abafe127bb905d266730cbc2817dc
Francois Lesueur <francois.lesueur@univ-ubs.fr>
2023-01-20 17:27:39 +0100

    use snster head

--- vagrant/files/provision.sh
+++ vagrant/files/provision.sh
@@ -100,7 +100,7 @@ iptables -A INPUT -p tcp --dport 3142 -i lxcbr0 -j ACCEPT # pour le proxy APT
 cd /root
 git clone https://framagit.org/flesueur/snster.git
 cd snster
-git checkout tags/v1.0.0-pre1
+# git checkout tags/v1.0.0-pre1
 ./install.sh
 
 # Provision MI-LXC
